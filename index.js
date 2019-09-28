9dbfce4ec957640f1382ae666668e4e1d8962cec
Sunhydra <dragoon6792@hotmail.com>
2019-09-27 22:19:55 -0700

    second

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+var http = require("http");
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-express().get('/', function(req, res) {
-res.sendFile(path.join(__dirname, 'views/pages/calculator.html'));
+const app = express();
+app.use(express.static(__dirname+'/views/pages'));
+app.get('/', function(req, res) {
+res.sendFile(__dirname+ '/views/pages/calculator.html');
 }) .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+setInterval(function() { http.get("http://<your app name>.herokuapp.com"); }, 300000); // every 5 minutes (300000)
